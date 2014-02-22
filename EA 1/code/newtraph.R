72d0e2122b4e9c18c19a2f907bbea2790445f9f2
andeek <andee.kaplan@gmail.com>
2014-02-22 14:48:54 -0600

    commenting

--- EA 1/code/newtraph.R
+++ EA 1/code/newtraph.R
@@ -1,13 +1,14 @@
-newtraph <- function(ders, dat, x0) {
+newtraph <- function(ders, dat, x0, n.iter = 1000) {
   ##function maximizes, for minization multiply likelihood by -1
 
   ##initialize critical values, initial values, etc.
-  crit <- c(1e-10, 1e-06, 1e-06, 1000)
+  crit <- c(1e-10, 1e-06, 1e-06, n.iter, 10)
   stopped <- rep(0,3)
   curnt <- x0
   p<-length(x0)
   k <- 1
   
+  ##run until 3 convergence criteria met or number of iterations exceeded
   while (sum(stopped) < 3 | k < crit[4]) {
     k <- k + 1
     int <- ders(curnt, dat)
@@ -19,7 +20,8 @@ newtraph <- function(ders, dat, x0) {
     new <- curnt - step
     sc <- 1
     
-    while(sc < 10) {
+    ##step-halving
+    while(sc < crit[5]) {
       sc <- sc + 1
       check <- ders(new, dat)
       if(check[[1]] < logL) {new <- curnt - (1/sc) * step}
@@ -29,13 +31,12 @@ newtraph <- function(ders, dat, x0) {
         break
       }                        
     }
-    if(sc == 10) stop("Step halving not effective, try new starting values.")
+    if(sc == crit[5]) stop("Step halving not effective, try new starting values.")
           
-    dist1<-abs(newL-logL)
-    dist2 <- sqrt(sum((new - curnt)^2))
+    dist <- c(abs(newL-logL), sqrt(sum((new - curnt)^2)))
     
-    if(crit[1] > dist1){stopped[1] <- stopped[1] + 1} #convergence of loglik
-    if(crit[2] > dist2){stopped[2] <- stopped[2] + 1} #convergence of estimates
+    if(crit[1] > dist[1]){stopped[1] <- stopped[1] + 1} #convergence of loglik
+    if(crit[2] > dist[2]){stopped[2] <- stopped[2] + 1} #convergence of estimates
     if(sum(crit[3] > abs(newg)) == p){ stopped[3] <- stopped[3] + 1} # convergence for sum of derivs
     curnt <- new
   }
