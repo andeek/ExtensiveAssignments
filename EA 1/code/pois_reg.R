c1b551936486bd9f0ac2f40a5fbbb3a30b8d8ce9
majohnso <majohnso@iastate.edu>
2014-03-04 21:33:40 -0600

    pois edits1

--- EA 1/code/pois_reg.R
+++ EA 1/code/pois_reg.R
@@ -1,8 +1,9 @@
 #### Libraries ####
 library(lubridate)
-library(reshape2)
+library(reshape)
 library(plyr)
 library(ggplot2)
+theme_set(theme_bw(base_family="serif"))
 
 #### Data ####
 dat<-read.table(file='./GitHub/ExtensiveAssignments/EA 1/data/greenbeandat.txt', header=TRUE)[,c(1,3,4,5)]
@@ -16,21 +17,28 @@ samp_store<-function(n, seed, dat){
 }
 
 #### Poisson Regression ####
-pois_fits <- dlply(dat, .(store), function(x) {
+## Stores 7022 and 7055 have constant price! Discard ##
+pois_fits <- dlply(dat[!dat$store %in% c(7022, 7055),], .(store), function(x) {
   glm(data=x, mvm ~ price, family=poisson, maxit=50)
 })
 
+### Root MSE ###
 rmse<-function(x){
   sqrt(sum((x$y - fitted(x))^2/length(x$y)))
 }
 
 rmse_store<-round(as.numeric(sapply(pois_fits, rmse)),3)
 
+### Coefficients ###
+ests<-t(sapply(pois_fits, function(u) u$coef))
 
+df<-melt(ests)
+qplot(data=df, x=value) + facet_wrap(~X2, scales="free_x")
 
+### Which stores Price not significant ###
+signif<-t(sapply(pois_fits, function(u) as.numeric(summary(u)$coef[,4])))
 
-
-
+sum(apply(signif, 1, function(u) u[2] > 0.5))
 
 
 
