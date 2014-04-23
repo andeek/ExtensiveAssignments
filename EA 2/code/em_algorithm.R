e4cf9ce2cdda8d0ef7b7e694b0adcc4d9bc7b07f
majohnso <majohnso@iastate.edu>
2014-04-23 00:58:49 -0500

    code (will add writeup tomorrow)

---
+++ EA 2/code/em_algorithm.R
@@ -0,0 +1,72 @@
+phat_fun<-function(phi, l1, l2, y, n){
+  (phi*(n*l1)^y*exp(-l1*n)/factorial(y))/(phi*(n*l1)^y*exp(-l1*n)/factorial(y) + (1-phi)*(n*l2)^y*exp(-l2*n)/factorial(y))
+}
+
+hessian<-function(phi, l1, l2, y, n){
+  h<-phi*(l1*n)^(y)*exp(-l1*n)/factorial(y) + (1-phi)*(l2*n)^(y)*exp(-l2*n)/factorial(y)
+  dfdlambda<-function(l){
+    exp(-l*n)*n^y*l^(y-1)*(l-n)/factorial(y)
+  }
+  df2d2lambda<-function(l){
+    exp(-l*n)*n^y*l^(y-2)*(n+y*(l-n)-n*l*(l-n))/factorial(y)
+  }
+  f<-function(l){
+    exp(-l*n)*(n*l)^y/factorial(y)
+  }
+  
+  dhdl1<-phi*dfdlambda(l1)
+  dhdl2<-(1-phi)*dfdlambda(l2)
+  dhdphi<-f(l1) - f(l2)
+  d2hd2l1<-phi*df2d2lambda(l1)
+  d2hd2l2<-(1-phi)*df2d2lambda(l2)
+  d2hd2phi<-0
+  d2hdl1l2<-0
+  d2dl1dphi<-dfdlambda(l1)
+  d2dl2dphi<- -dfdlambda(l2)
+  
+  hess11<-sum(-dhdphi^2/h^2)
+  hess22<-sum(d2hd2l1/h - dhdl1^2/h^2)
+  hess33<-sum(d2hd2l2/h - dhdl2^2/h^2)
+  hess12<-sum(d2dl1dphi/h - dhdl1*dhdphi/h^2)
+  hess13<-sum(d2dl2dphi/h - dhdl2*dhdphi/h^2)
+  hess23<-sum(d2hdl1l2/h - dhdl1*dhdl2/h^2)
+  hessian<-matrix(c(hess11, hess12, hess13, hess12, hess22, hess23, hess13, hess23, hess33), ncol=3)
+  return(hessian)
+}
+
+em_algorithm<-function(phi, l1, l2, y, n, maxit=1000, conv.crit=10e-8){
+  j<-1
+  repeat{
+    phat<-phat_fun(phi, l1, l2, y, n)
+    phi2<-mean(phat)
+    l1_2<-sum(y*phat)/sum(n*phat)
+    l2_2<-sum(y*(1-phat))/sum(n*(1-phat))
+    if(abs(phi2-phi) < conv.crit & abs(l1_2-l1) < conv.crit & abs(l2_2-l2) < conv.crit){
+      cat("Convergence reached\n")
+      break 
+    } 
+    if(j == maxit){
+      cat("Maximum iterations reached\n")
+      break
+    }else{
+      j<-j+1
+      phi<-phi2
+      l1<-l1_2
+      l2<-l2_2
+    }
+  }
+  return(c(phi=phi2, lambda1=l1, lambda2=l2, n.iter=j))
+}
+
+em_algorithm(.1, .3, 6, y=flaws$flaws, n=flaws$length)
+
+ests<-em_algorithm(.5, 5, 0.01, y=flaws$flaws, n=flaws$length)
+## Match mles from optim
+## So, how do we get probability each observation is in group A?
+probs<-round(phat_fun(ests[1], ests[2], ests[3], y=flaws$flaws, n=flaws$length),3)
+qplot(x=flaws$flaws, y=probs)
+flaws$probs<-probs
+
+hessian(ests[1], ests[2], ests[3], y=flaws$flaws, n=flaws$length)
+# I think there must be a mistake somewhere, one positive two negative. Too tired, will check tomorrow.
+
---
+++ EA 2/code/profile_poisson.R
@@ -0,0 +1,68 @@
+#### Libraries ####
+library(ggplot2)
+library(reshape2)
+
+#### Data ####
+flaws<-read.table("subwaydat.txt", header=TRUE)
+flaws$rate<-flaws$flaws/flaws$length
+
+############# Estimation #############
+
+#### Homogeneous Model ####
+## Likelihood ##
+simple_lik<-function(lambda, y, n){
+  sum(log((lambda*n)^y*exp(-lambda*n)))
+}
+
+## MLEs
+lambda.est<-optim(.5, function(u) -simple_lik(u, y=flaws$flaws, n=flaws$length), hessian=TRUE)
+ci<-c(lambda.est$par - qnorm(0.975)*1/sqrt(lambda.est$hessian), lambda.est$par + qnorm(0.975)*1/sqrt(lambda.est$hessian))
+
+## Get Predictions 
+flaws$exp.counts <- lambda.est$par*flaws$length
+ddply(data=flaws, .(length), summarize, flaws=sum(flaws), exp.counts=sum(exp.counts))
+
+### Profile phi ###
+## I think I need constraints on lambda1, lambda2, i.e. lambda1 > lambda2 ###
+likelihood_pois<-function(par, phi, y, n){
+  l1<-par[1]
+  l2<-par[2]
+  ifelse(l1 > l2 & l1 >0 & l2>0, sum(log(phi*(l1*n)^(y)*exp(-l1*n) + (1-phi)*(l2*n)^(y)*exp(-l2*n))), -1e50)
+}
+
+Lp_pois<-function(phi, y, n){
+  mles <- optim(par0s, function(a){-likelihood_pois(a, phi=phi, y=y, n=n)})$par
+  lp<-likelihood_pois(mles, y=y, phi=phi, n=n)
+  par0s<<-mles
+  return(c(l1=mles[1], l2=mles[2], phi=phi, lp=lp))
+}
+
+Lp_pois2<-function(phi, y, n){
+  mles <- optim(par0s, function(a){-likelihood_pois(a, phi=phi, y=y, n=n)})$par
+  lp<-likelihood_pois(mles, y=y, phi=phi, n=n)
+  par0s<<-mles
+  return(lp)
+}
+
+phi.est<-optimize(function(u) Lp_pois2(u, y=flaws$flaws, n=flaws$length), interval=c(0,1), maximum=TRUE)$max
+
+optim(c(2,1), function(a) -likelihood_pois(a, phi=phi.est, y=flaws$flaws, n=flaws$length))
+## phi = 0.4967
+
+par0s<-c(5,1)
+phi<-matrix(seq(0,1,.0001),ncol=1)
+df<-as.data.frame(t(apply(phi, 1, function(u) Lp_pois(u, y=flaws$flaws, n=flaws$length))))  
+df[which.max(df$lp),]
+#       l1        l2    phi       lp
+# 3.158086 0.5266318 0.4967 582.3936
+
+
+## No profile ##
+likelihood_pois2<-function(par, y, n){
+  l1<-par[1]
+  l2<-par[2]
+  phi<-par[3]
+  ifelse(l1 > l2 & l1 >0 & l2>0, sum(log(phi*(l1*n)^(y)*exp(-l1*n) + (1-phi)*(l2*n)^(y)*exp(-l2*n))), -1e50)
+}
+
+optim(c(2,1,.4), function(u) -likelihood_pois2(u, y=flaws$flaws, n=flaws$length))
